44727c5966e7f5b39a4a31f9b444f1b2470274d3
nu <xaver.wenhart@proton.me>
2025-06-15 00:45:42 +0200

    fix: logic error

--- default.nix
+++ default.nix
@@ -1,13 +1,25 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  pkgs ? import <nixpkgs> {},
+}:
 pkgs.stdenv.mkDerivation (finalAttrs: {
   pname = "extract";
   version = "1.0.0";
   src = ./src;
-  buildInputs = [ ];
+  buildInputs = [ pkgs.makeWrapper ];
   installPhase = ''
     mkdir -p $out/bin
     cp extract $out/bin/extract
     chmod +x $out/bin/extract
+    wrapProgram $out/bin/extract \
+      --prefix PATH : ${pkgs.gnutar}/bin \
+      --prefix PATH : ${pkgs.xz}/bin \
+      --prefix PATH : ${pkgs.bzip2}/bin \
+      --prefix PATH : ${pkgs.gzip}/bin \
+      --prefix PATH : ${pkgs.unzip}/bin \
+      --prefix PATH : ${pkgs.ncompress}/bin \
+      --prefix PATH : ${pkgs.p7zip}/bin \
+      --prefix PATH : ${pkgs.cabextract}/bin \
+      --prefix PATH : ${pkgs.cpio}/bin \
   '';
 
   #meta = {
